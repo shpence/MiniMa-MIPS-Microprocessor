1c05b7c91d2380dde32efa77e0cb26898104bd0e
Armstrong, Spencer M <spencer-armstrong@uiowa.edu>
2020-04-04 11:35:13 -0500

    Phase 1 Fixes

--- Phase2.c
+++ Phase2.c
@@ -1,6 +1,9 @@
 #include "Phase2.h"
 
 void resolve_addresses(struct ArrayList *unresolved, uint32_t first_pc, struct ArrayList *resolved) {
-
+	struct ArrayList* labels = newArrayList();
+	for (int i = 0; i < unresolved->size; i++) {
+		if (get(unresolved, i).label != "")
+	}
 }
 
